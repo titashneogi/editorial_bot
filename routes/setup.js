f71c0ecf6f051d9dc7353f485f0be18aa4b27bf5
himanshu sharma <himanshu@himanshus-MacBook-Pro.local>
2016-07-07 10:01:24 +0530

    added files in git ignore

--- routes/setup.js
+++ routes/setup.js
@@ -6,10 +6,6 @@ function setup(req, res) {
 	console.log(req.body);
 	console.log(CUSTOM_PHRASE_LOC.numberOFStories);
   	res.send('success');
-  	FS.writeFile('helloworld.txt', 'Hello World!', function (err) {
-	  if (err) return console.log(err);
-	  console.log('Hello World > helloworld.txt');
-	});
   	FS.writeFile(CUSTOM_PHRASE_LOC, JSON.stringify(req.body),null, function(err) {
       if (err) {
         console.error('Error while writing new serialized phrase object.');
